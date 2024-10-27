b807c5e5f020406ef696957f792c842bcbbb1064
Faishal Irawan <muhammadfaishal193@gmail.com>
2024-10-27 16:02:46 +0700

    add slideshow section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { ChevronDown } from "@/components/ui/icons/ChevronDown";
 import { Music } from "@/components/ui/icons/Music";
 import { MusicOff } from "@/components/ui/icons/MusicOff";
 import {
@@ -14,6 +15,28 @@ import {
     VStack,
 } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
+
+const Slide = ({
+    backgroundUrl,
+    animationDelay,
+}: {
+    backgroundUrl: string;
+    animationDelay: string;
+}) => {
+    return (
+        <Box
+            pos={"absolute"}
+            w={"full"}
+            h={"full"}
+            opacity={"0"}
+            animation={"slide 25s infinite"}
+            animationDelay={animationDelay}
+            background={`linear-gradient(rgba(50, 48, 48, 0.5), rgba(50, 48, 48, 0.5)), url(${backgroundUrl})`}
+            backgroundPosition={"50%"}
+            backgroundSize={"cover"}
+        ></Box>
+    );
+};
 
 export default function Home() {
     const [clicked, setClicked] = useState(false);
@@ -112,65 +135,163 @@ export default function Home() {
                     </Text>
                 </Box>
             </Box>
-            <Box
-                w="500px"
-                bgColor="grey"
-                background={
-                    "linear-gradient(rgb(50 48 48/ 50%), rgb(50 48 48 / 50%)),center/cover url(https://ik.imagekit.io/drpq5xrph/Template%20Tiffany%20&%20Jared/1.%20Cover.jpg?updatedAt=1698222296920)"
-                }
-            >
-                <VStack
-                    minH="100vh"
-                    textAlign={"center"}
-                    justifyContent={"space-between"}
-                >
-                    <Heading
-                        fontFamily="headingAlternative"
-                        letterSpacing={"2px"}
-                        fontSize="md"
-                        lineHeight={"1.5"}
-                        fontWeight={"700"}
-                        mt={"92px"}
+            <Box w="500px">
+                <Show when={!init}>
+                    <VStack
+                        minH="100vh"
+                        textAlign={"center"}
+                        justifyContent={"space-between"}
+                        pos={"relative"}
+                        background={
+                            "linear-gradient(rgb(50 48 48/ 50%), rgb(50 48 48 / 50%)),center/cover url(https://ik.imagekit.io/drpq5xrph/Template%20Tiffany%20&%20Jared/1.%20Cover.jpg?updatedAt=1698222296920)"
+                        }
                     >
-                        WEDDING ANNOUNCEMENT
-                    </Heading>
-                    <Box mt={"130px"}>
                         <Heading
-                            fontSize="4xl"
-                            fontWeight="light"
-                            lineHeight={"1.2"}
-                            textTransform={"uppercase"}
+                            fontFamily="headingAlternative"
+                            letterSpacing={"2px"}
+                            fontSize="md"
+                            lineHeight={"1.5"}
+                            fontWeight={"700"}
+                            mt={"92px"}
                         >
-                            Tiffany & Jared
+                            WEDDING ANNOUNCEMENT
                         </Heading>
-                        <Heading
-                            fontSize="4xl"
-                            fontWeight="light"
-                            lineHeight={"1.2"}
+                        <Box
+                            position="absolute"
+                            top="48%"
+                            transform="translateY(-50%)"
+                        >
+                            <Heading
+                                fontSize="4xl"
+                                fontWeight="light"
+                                lineHeight={"1.2"}
+                                textTransform={"uppercase"}
+                            >
+                                Tiffany & Jared
+                            </Heading>
+                            <Heading
+                                fontSize="4xl"
+                                fontWeight="light"
+                                lineHeight={"1.2"}
+                                fontFamily={"body"}
+                                fontStyle={"italic"}
+                            >
+                                #TImetoshaRE
+                            </Heading>
+                        </Box>
+                        <Button
+                            mb={"250px"}
+                            h={"2rem"}
+                            p={"0 2.4rem"}
                             fontFamily={"body"}
                             fontStyle={"italic"}
+                            fontSize={"lg"}
+                            borderRadius={"0"}
+                            border={"1px solid #1A1B1D"}
+                            backgroundColor={"bgSecondary"}
+                            color={"secondaryColorText"}
+                            animation={"MoveUpDown 3s linear infinite"}
+                            loading={clicked}
+                            onClick={() => setClicked(true)}
                         >
-                            #TImetoshaRE
-                        </Heading>
+                            Open
+                        </Button>
+                    </VStack>
+                </Show>
+                <Show when={init}>
+                    <Box h="100vh" pos={"relative"}>
+                        <Box
+                            pos={"absolute"}
+                            w={"full"}
+                            h={"full"}
+                            top={"0"}
+                            left={"0"}
+                            zIndex={"-1"}
+                            overflow={"hidden"}
+                        >
+                            <Slide
+                                animationDelay="0s"
+                                backgroundUrl="https://ik.imagekit.io/drpq5xrph/Template%20Tiffany%20&%20Jared/1.%20Cover.jpg?updatedAt=1698222296920"
+                            />
+                            <Slide
+                                animationDelay="5s"
+                                backgroundUrl="https://ik.imagekit.io/drpq5xrph/Template%20Tiffany%20&%20Jared/Slideshow/Slideshow-Cover-2.jpg?updatedAt=1698222443322"
+                            />
+                            <Slide
+                                animationDelay="10s"
+                                backgroundUrl="https://ik.imagekit.io/drpq5xrph/Template%20Tiffany%20&%20Jared/Slideshow/Slideshow-Cover-3.jpg?updatedAt=1698222442828"
+                            />
+                            <Slide
+                                animationDelay="15s"
+                                backgroundUrl="https://ik.imagekit.io/drpq5xrph/Template%20Tiffany%20&%20Jared/Slideshow/Slideshow-Cover-4.jpg?updatedAt=1698222541497"
+                            />
+                            <Slide
+                                animationDelay="20s"
+                                backgroundUrl="https://ik.imagekit.io/drpq5xrph/Template%20Tiffany%20&%20Jared/Slideshow/Slideshow-Cover-5.jpg?updatedAt=1698222444372"
+                            />
+                        </Box>
+
+                        <VStack
+                            h={"full"}
+                            textAlign={"center"}
+                            justifyContent={"space-between"}
+                            zIndex={"2"}
+                        >
+                            <Heading
+                                fontFamily="headingAlternative"
+                                letterSpacing={"2px"}
+                                fontSize="md"
+                                lineHeight={"1.5"}
+                                fontWeight={"700"}
+                                mt={"92px"}
+                            >
+                                WEDDING ANNOUNCEMENT
+                            </Heading>
+                            <Box
+                                position="absolute"
+                                top="48%"
+                                transform="translateY(-50%)"
+                            >
+                                <Heading
+                                    fontSize="4xl"
+                                    fontWeight="light"
+                                    lineHeight={"1.2"}
+                                    textTransform={"uppercase"}
+                                >
+                                    Tiffany & Jared
+                                </Heading>
+                                <Heading
+                                    fontSize="4xl"
+                                    fontWeight="light"
+                                    lineHeight={"1.2"}
+                                    fontFamily={"body"}
+                                    fontStyle={"italic"}
+                                >
+                                    #TImetoshaRE
+                                </Heading>
+                            </Box>
+                            <Flex
+                                w="full"
+                                paddingRight={"2rem"}
+                                mb={"72px"}
+                                justifyContent={"flex-end"}
+                                alignItems={"center"}
+                                cursor={"pointer"}
+                            >
+                                <Text
+                                    fontFamily={"headingAlternative"}
+                                    marginRight={"4px"}
+                                    fontSize={"sm"}
+                                >
+                                    SCROLL TO BEGIN
+                                </Text>
+                                <Center>
+                                    <ChevronDown width={"28"} height={"28"} />
+                                </Center>
+                            </Flex>
+                        </VStack>
                     </Box>
-                    <Button
-                        mb={"250px"}
-                        h={"2rem"}
-                        p={"0 2.4rem"}
-                        fontFamily={"body"}
-                        fontStyle={"italic"}
-                        fontSize={"lg"}
-                        borderRadius={"0"}
-                        border={"1px solid #1A1B1D"}
-                        backgroundColor={"bgSecondary"}
-                        color={"secondaryColorText"}
-                        animation={"MoveUpDown 3s linear infinite"}
-                        loading={clicked}
-                        onClick={() => setClicked(true)}
-                    >
-                        Open
-                    </Button>
-                </VStack>
+                </Show>
             </Box>
             <Show when={init}>
                 <IconButton
