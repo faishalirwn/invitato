b205153e9cf5b1648b88b11dcd09d07695ed7d1e
Faishal Irawan <muhammadfaishal193@gmail.com>
2024-10-27 18:13:21 +0700

    remove color mode in provider

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ColorModeProvider } from "./color-mode";
 import { system } from "@/styles/theme";
 
 export function Provider(props: React.PropsWithChildren) {
-    return (
-        <ChakraProvider value={system}>
-            {/* <ColorModeProvider>{props.children}</ColorModeProvider> */}
-            {props.children}
-        </ChakraProvider>
-    );
+    return <ChakraProvider value={system}>{props.children}</ChakraProvider>;
 }
